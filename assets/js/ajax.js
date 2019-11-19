d6acc47f34c4f72da4798151acd8d0027a8d762d
daniehao <you@example.com>
2019-11-18 20:30:25 -0500

    login and navbar

---
+++ assets/js/ajax.js
@@ -0,0 +1,42 @@
+import store from './store';
+export function post_login(path, body) {
+  return fetch('/ajax' + path, {
+    method: 'post',
+    credentials: 'same-origin',
+    headers: new Headers({
+      'x-csrf-token': window.csrf_token,
+      'content-type': "application/json; charset=UTF-8",
+      'accept': 'application/json',
+    }),
+    body: JSON.stringify(body),
+  }).then((resp) => resp.json());
+}
+
+
+export function submit_login(form) {
+  let state = store.getState();
+  let data = state.forms.login;
+  console.log("submit data:", data);
+
+  post_login('/sessions', data)
+    .then((resp) => {
+      console.log(resp);
+      if (resp.token) {
+        localStorage.setItem('session', JSON.stringify(resp));
+        store.dispatch({
+          type: 'LOG_IN',
+          data: resp,
+        });
+        form.redirect('/');
+      }
+      else {
+        store.dispatch({
+          type: 'CHANGE_LOGIN',
+          data: {errors: JSON.stringify(resp.errors)},
+        });
+      }
+    });
+}
+
+
+
---
+++ lib/recipe_app_web/controllers/session_controller.ex
@@ -0,0 +1,24 @@
+
+defmodule RecipeAppWeb.SessionController do
+  use RecipeAppWeb, :controller
+
+  action_fallback RecipeAppWeb.FallbackController
+
+  alias RecipeApp.Users
+
+  def create(conn, %{"email" => email, "password" => password}) do
+    user = Users.authenticate_user(email, password)
+    if user do
+      token = Phoenix.Token.sign(conn, "session", user.id)
+      resp = %{token: token, user_id: user.id, user_name: user.name, email: user.email}
+      conn
+      |> put_resp_header("content-type", "application/json; charset=UTF-8")
+      |> send_resp(:created, Jason.encode!(resp))
+    else
+      resp = %{errors: ["Authentication Failed"]}
+      conn
+      |> put_resp_header("content-type", "application/json; charset=UTF-8")
+      |> send_resp(:unauthorized, Jason.encode!(resp))
+    end
+  end
+end
