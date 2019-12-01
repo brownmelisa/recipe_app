ec7d1b0b3c71d0368a82d9acc2cc3966742bf51e
Melisa Brown <melisa@Melisas-MacBook-Pro.local>
2019-12-01 13:21:35 -0500

    sure

--- lib/recipe_app/search_recipes.ex
+++ lib/recipe_app/search_recipes.ex
@@ -25,7 +25,7 @@ defmodule RecipeApp.SearchRecipesApi do
   # make api specific changes to params
   def validateParams(params) do
     params = Map.put(params, "apiKey", @apiKey)
-             |> Map.put("number", 5) # no of results returned
+             |> Map.put("number", 9) # no of results returned
       # api does not return nutrition information w/o below params
              |> Map.put("minFat", 0)
              |> Map.put("minCarbs", 0)
